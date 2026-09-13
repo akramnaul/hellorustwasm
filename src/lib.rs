f3ab4ce3abec567f6e69434371c37d34aaf47481
akramnaul <60735293+akramnaul@users.noreply.github.com>
2026-09-14 02:14:53 +0500

    Implement HTTP component to serve Hello World page

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+```rust
+use spin_sdk::http::{IntoResponse, Request, Response};
+use spin_sdk::http_component;
+
+#[http_component]
+fn handle_hello_world(_req: Request) -> anyhow::Result<impl IntoResponse> {
+
+    let html = r#"<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+
+    <title>Hello Rust WASM</title>
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            margin-top: 100px;
+        }
+
+        h1 {
+            font-size: 42px;
+        }
+
+        p {
+            font-size: 22px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h1>Hello, World!</h1>
+
+    <p>
+        Hello, World ! ... Rust .WASM in the Browser
+    </p>
+
+</body>
+
+</html>"#;
+
+    Ok(
+        Response::builder()
+            .status(200)
+            .header(
+                "content-type",
+                "text/html; charset=utf-8"
+            )
+            .body(html)
+            .build()
+    )
+}
+```
